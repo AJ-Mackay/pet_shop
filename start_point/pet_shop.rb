d6b98780050167979f2100cde775d8f9e1142e8c
Ruiner85 <paul.m.mackay85@gmail.com>
2020-01-17 21:03:27 +0000

    add add or remove part one

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -5,3 +5,8 @@ end
 def total_cash(money)
   return money[:admin][:total_cash]
 end
+
+def add_or_remove_cash(pet_shop, amount)
+  result = pet_shop[:admin][:total_cash] += amount
+  return result
+end
