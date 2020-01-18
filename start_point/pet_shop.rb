978a8ea7bed91014fd7526d50b1541398f0c7c76
Ruiner85 <paul.m.mackay85@gmail.com>
2020-01-18 16:31:34 +0000

    add customer can afford pet test three

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -75,7 +75,5 @@ def add_pet_to_customer(customer, pet)
 end
 
 def customer_can_afford_pet(customer, pet)
-  if customer[:cash] >= pet[:price]
-    return true
-  end
+  return customer[:cash] >= pet[:price]
 end
