a097d114319a9b57b4ef5b31ec3327796d5ee862
Ruiner85 <paul.m.mackay85@gmail.com>
2020-01-18 21:44:22 +0000

    add sell pet to customer test part two

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -79,9 +79,11 @@ def customer_can_afford_pet(customer, pet)
 end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
-  customer[:pets] << pet
-  pet_shop[:pets].delete(pet)
-  pet_shop[:admin][:pets_sold] += 1
-  customer[:cash] -= pet[:price]
-  pet_shop[:admin][:total_cash] += pet[:price]
-end
+    if pet_shop[:pets].include?(pet)
+      customer[:pets] << pet
+      pet_shop[:pets].delete(pet)
+      pet_shop[:admin][:pets_sold] += 1
+      customer[:cash] -= pet[:price]
+      pet_shop[:admin][:total_cash] += pet[:price]
+    end
+  end
